f539c86fd8b234859d2194d9f3e56bd04e5455d1
barsovichok <taya.kulagina@gmail.com>
2018-07-31 13:28:15 +0300

    add redis storage

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import scoring_repos
 from flask import Flask, request, jsonify
+import redis
+
 
 app = Flask(__name__)
 
@@ -9,15 +11,26 @@ def evaluate_repo():
 
     owner = request.args.get('owner', type=str)
     namerepo = request.args.get('namerepo', type=str)
-    repository = owner+'/'+namerepo
-
-    check_input = scoring_repos.check_user_input(repository)
-    if check_input is None:
-        return jsonify(
-            error='Invalid values, please try again')
+    redis_repo = owner+'_'+namerepo
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    check_redis_repo = r.get(redis_repo)
+    if check_redis_repo is None:
+        repository = owner+'/'+namerepo
+        check_input = scoring_repos.check_user_input(repository)
+        if check_input is None:
+            return jsonify(
+                error='Invalid values, please try again')
+        else:
+            repo_result = scoring_repos.eval_repository(repository)
+            insert_redis_result = r.set(redis_repo,repo_result)
+            return jsonify(rate=str(repo_result))
     else:
-        repo_result = scoring_repos.eval_repository(repository)
-        return jsonify(rate=str(repo_result))
+        check_redis_repo = check_redis_repo.decode('utf-8')
+        return jsonify(rate=str(check_redis_repo))
+
+
+    
+    
 
 
 if __name__ == "__main__":
