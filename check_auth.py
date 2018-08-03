0d503a4bd91ef0e7d66925c6bcb2e27c0476abb6
barsovichok <taya.kulagina@gmail.com>
2018-08-03 10:57:31 +0300

    add 401 status code for non-authorized requests

--- check_auth.py
+++ check_auth.py
@@ -1,13 +1,11 @@
 import token_generator
-from flask import jsonify
+from flask import jsonify, Response
 
 
 def check_auth_token(token):
     redis_base = token_generator.create_redis_base()
     check_redis_token = redis_base.get(token)
     if check_redis_token is None:
-        return jsonify(
-            error='You aren\'t authorized, pls email taya.kulagina@gmail.com'
-            )
+        return  Response("{'error':'You aren\'t authorized, pls email taya.kulagina@gmail.com'}", status=401, mimetype='application/json')
     else:
         return None
