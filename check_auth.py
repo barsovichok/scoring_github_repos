530ecb5182f41aa24cf7cd65d3998c4cd62070c5
barsovichok <taya.kulagina@gmail.com>
2018-08-03 11:07:25 +0300

    add form json object using json.dumps()

--- check_auth.py
+++ check_auth.py
@@ -1,11 +1,13 @@
 import token_generator
-from flask import jsonify, Response
+from flask import Response
+import json
 
 
 def check_auth_token(token):
     redis_base = token_generator.create_redis_base()
     check_redis_token = redis_base.get(token)
     if check_redis_token is None:
-        return  Response("{'error':'You aren\'t authorized, pls email taya.kulagina@gmail.com'}", status=401, mimetype='application/json')
+        auth_error = json.dumps({'error':'You aren\'t authorized, pls email taya.kulagina@gmail.com'})
+        return  Response(auth_error, status=401, mimetype='application/json')
     else:
         return None
